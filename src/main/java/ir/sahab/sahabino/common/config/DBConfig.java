ef2dc2a5d907c58d2435c99993c7768ee9a8fcb0
mojak <javadakbari1379@gmail.com>
2022-09-12 15:57:59 +0430

    new config file(s)

--- src/main/java/ir/sahab/sahabino/common/config/DBConfig.java
+++ src/main/java/ir/sahab/sahabino/common/config/DBConfig.java
@@ -1,8 +1,23 @@
 package ir.sahab.sahabino.common.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class DBConfig {
-    public final static String DB_USER_NAME = "root";
-    public final static String DB_USER_PASS = "root";
-    public final static String DB_URL = "jdbc:mysql://localhost/";
-    public final static String DB_NAME = "Notification";
+
+    public static Properties PROPS = new Properties();
+    static {
+        try {
+            InputStream input = new FileInputStream("src/main/resources/database.properties");
+            PROPS.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public final static String DB_USER_NAME = PROPS.getProperty("DB_USER_NAME");
+    public final static String DB_USER_PASS = PROPS.getProperty("DB_USER_PASS");
+    public final static String DB_URL = PROPS.getProperty("DB_URL");
+    public final static String DB_NAME = PROPS.getProperty("DB_NAME");
 }
--- src/main/java/ir/sahab/sahabino/common/config/KafkaConfig.java
+++ src/main/java/ir/sahab/sahabino/common/config/KafkaConfig.java
@@ -2,14 +2,27 @@ package ir.sahab.sahabino.common.config;
 
 import ir.sahab.sahabino.rulesEvaluator.Rules.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class KafkaConfig {
-    public static final String KAFKA_TOPIC = "logQueue";
-    public static final String KAFKA_BOOTSTRAP_SERVERS = "localhost:9092";
-    public static final String KAFKA_CLIENT_ID = "FileBeatLogQueue";
-    public static final String WATCHING_FOLDER_ADDRESS = "/home/mojak/Desktop/job/sahab/log-test-folder";
-    public static final String RULE_FILE_ADDRESS = "src/main/java/ir/sahab/sahabino/rulesEvaluator/Rules/rules.json";
-    public static final String KAFKA_GROUP_ID = "FileBeatLogQueue";
+    public static Properties PROPS = new Properties();
+    static {
+        try {
+            InputStream input = new FileInputStream("src/main/resources/kafka.properties");
+            PROPS.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static final String KAFKA_TOPIC = PROPS.getProperty("KAFKA_TOPIC");
+    public static final String KAFKA_BOOTSTRAP_SERVERS = PROPS.getProperty("KAFKA_BOOTSTRAP_SERVERS");
+    public static final String KAFKA_CLIENT_ID = PROPS.getProperty("KAFKA_CLIENT_ID");
+    public static final String KAFKA_GROUP_ID = PROPS.getProperty("KAFKA_GROUP_ID");
+    public static final String WATCHING_FOLDER_ADDRESS = PROPS.getProperty("WATCHING_FOLDER_ADDRESS");
+    public static final String RULE_FILE_ADDRESS = PROPS.getProperty("RULE_FILE_ADDRESS");
 
 }
